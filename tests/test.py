0b8b48fe1a4f4669eac5c2d70bc2f43b270a86f6
cagbal <cagatayodabasi91@gmail.com>
2019-06-12 10:31:32 +0200

    Flushall added to the tests

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 import unittest
 
+import redis
+
 from robotDBRedis.modules import User
+
+def flushall():
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    r.flushall()
+
 
 class UserTest(unittest.TestCase):
 
     def test_user_name(self):
+        flushall()
+
         user0 = User("Cagatay")
         user1 = User("Mark")
         user2 = User("Daisy")
@@ -22,7 +31,11 @@ class UserTest(unittest.TestCase):
         self.assertEqual(user5.get_name(), "bazzsi")
         self.assertEqual(user6.get_name(), "jacob")
 
+
+
     def test_user_hash(self):
+        flushall()
+
         user0 = User("Cagatay")
         user1 = User("Mark")
         user2 = User("Daisy")
@@ -31,10 +44,10 @@ class UserTest(unittest.TestCase):
         user5 = User("Bazzi")
         user6 = User("Jacob")
 
-        self.assertEqual(user0.get_hash(), "user:00000")
-        self.assertEqual(user1.get_hash(), "user:00001")
-        self.assertEqual(user2.get_hash(), "user:00002")
-        self.assertEqual(user3.get_hash(), "user:00003")
-        self.assertEqual(user4.get_hash(), "user:00004")
-        self.assertEqual(user5.get_hash(), "user:00005")
-        self.assertEqual(user6.get_hash(), "user:00006")
+        self.assertEqual(user0.get_hash(), "user:00001")
+        self.assertEqual(user1.get_hash(), "user:00002")
+        self.assertEqual(user2.get_hash(), "user:00003")
+        self.assertEqual(user3.get_hash(), "user:00004")
+        self.assertEqual(user4.get_hash(), "user:00005")
+        self.assertEqual(user5.get_hash(), "user:00006")
+        self.assertEqual(user6.get_hash(), "user:00007")
