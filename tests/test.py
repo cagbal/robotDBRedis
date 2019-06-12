38b8326a1510fed59f2b258af0a942cbdd4600ce
cagbal <cagatayodabasi91@gmail.com>
2019-06-12 10:15:44 +0200

    get hash test added

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,40 @@
 #!/usr/bin/python
 import unittest
 
-
 from robotDBRedis.modules import User
 
-class CreateAUserTest(unittest.TestCase):
-    def test_name(self):
-        user = User("Cagatay")
-        self.assertEqual(user.get_name(), "cagatay")
+class UserTest(unittest.TestCase):
+
+    def test_user_name(self):
+        user0 = User("Cagatay")
+        user1 = User("Mark")
+        user2 = User("Daisy")
+        user3 = User("Baron")
+        user4 = User("Bruno")
+        user5 = User("Bazzi")
+        user6 = User("Jacob")
+
+        self.assertEqual(user0.get_name(), "cagatay")
+        self.assertEqual(user1.get_name(), "mark")
+        self.assertEqual(user2.get_name(), "daisy")
+        self.assertEqual(user3.get_name(), "baron")
+        self.assertEqual(user4.get_name(), "bruno")
+        self.assertEqual(user5.get_name(), "bazzsi")
+        self.assertEqual(user6.get_name(), "jacob")
+
+    def test_user_hash(self):
+        user0 = User("Cagatay")
+        user1 = User("Mark")
+        user2 = User("Daisy")
+        user3 = User("Baron")
+        user4 = User("Bruno")
+        user5 = User("Bazzi")
+        user6 = User("Jacob")
+
+        self.assertEqual(user0.get_hash(), "user:00000")
+        self.assertEqual(user1.get_hash(), "user:00001")
+        self.assertEqual(user2.get_hash(), "user:00002")
+        self.assertEqual(user3.get_hash(), "user:00003")
+        self.assertEqual(user4.get_hash(), "user:00004")
+        self.assertEqual(user5.get_hash(), "user:00005")
+        self.assertEqual(user6.get_hash(), "user:00006")
