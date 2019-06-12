b478611342b44fb49186b5ccb2a86b6c2299e374
cagbal <cagatayodabasi91@gmail.com>
2019-06-12 17:26:27 +0200

    Example added

--- drink_serving_robot_example.py
+++ drink_serving_robot_example.py
@@ -2,11 +2,28 @@
 
 # Drink Serving Robot Example
 
-from robotDBRedis import User
+from robotDBRedis.modules import User
+from robotDBRedis.fields import ListField
 
 class DrinkServingRobotUser(User):
     """docstring for DrinkServingRobotUser.
     This is a custom User to use"""
-    def __init__(self, arg):
-        super(DrinkServingRobotUser, self).__init__()
-        self.arg =
+    def __init__(self, name="user", drink_pref_list = [],
+                 module_name = "drink_serving_user",
+                 host='localhost', port=6379, db=0):
+        super(DrinkServingRobotUser, self).__init__(name=name,
+                     module_name = module_name,
+                     host=host, port=port, db=db)
+
+        self._drink_preferences = \
+                self.add_field(ListField("drinks_pref", drink_pref_list))
+
+        self.push()
+
+    def get_drink_prefs(self):
+        return self._drink_preferences.get()
+
+
+user = DrinkServingRobotUser("cagatay", ["apple, coffee"])
+
+print(user.get_drink_prefs())
