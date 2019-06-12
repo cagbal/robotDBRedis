bb20d290eaf5041c05722a5bb0010e68393db067
cagbal <cagatayodabasi91@gmail.com>
2019-06-12 15:23:16 +0200

    Example file name changed

---
+++ drink_serving_robot_example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# Drink Serving Robot Example
+
+from robotDBRedis import User
+
+class DrinkServingRobotUser(User):
+    """docstring for DrinkServingRobotUser.
+    This is a custom User to use"""
+    def __init__(self, arg):
+        super(DrinkServingRobotUser, self).__init__()
+        self.arg =
